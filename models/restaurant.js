025c993f62e51c4693ee9b8163bfebbfed45e7f1
futurenowchen <futurenowchen@gmail.com>
2021-08-30 23:05:16 +0800

    Show category name on admin/restaurants & admin/restaurant

--- models/restaurant.js
+++ models/restaurant.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
     address: DataTypes.STRING,
     opening_hours: DataTypes.STRING,
     description: DataTypes.TEXT,
-    image: DataTypes.STRING
+    image: DataTypes.STRING,
     CategoryId: DataTypes.INTEGER
   }, {
     sequelize,
