8b1a340bd62af68921625366b948aba8c69a3cfc
futurenowchen <futurenowchen@gmail.com>
2021-08-31 23:53:59 +0800

    Update category select in edit page using ifCond-helper

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const db = require('./models')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }, helpers: require('./config/handlebars-helpers')))
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
