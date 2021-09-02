c6653f839712ca2bfd4b2401a53fd6f27c774b96
futurenowchen <futurenowchen@gmail.com>
2021-09-02 20:50:23 +0800

    Add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,17 +1,53 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
+
 
 const restController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category }).then(restaurants => {
-      const data = restaurants.map(r => ({
+    let offset = 0
+    const whereQuery = {}
+    let categoryId = ''
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery.categoryId = categoryId
+    }
+    Restaurant.findAndCountAll({
+      include: Category,
+      where: whereQuery,
+      offset: offset,
+      limit: pageLimit
+    }).then(result => {
+      // data for pagination
+      const page = Number(req.query.page) || 1
+      const pages = Math.ceil(result.count / pageLimit)
+      const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      const prev = page - 1 < 1 ? 1 : page - 1
+      const next = page + 1 > pages ? pages : page + 1
+
+      // clean up restaurant data
+      const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
-        categoryName: r.Category.name
+        categoryName: r.dataValues.Category.name
       }))
-      return res.render('restaurants', {
-        restaurants: data
+      Category.findAll({
+        raw: true,
+        nest: true
+      }).then(categories => {
+        return res.render('restaurants', {
+          restaurants: data,
+          categories: categories,
+          categoryId: categoryId,
+          page: page,
+          totalPage: totalPage,
+          prev: prev,
+          next: next
+        })
       })
     })
   },
